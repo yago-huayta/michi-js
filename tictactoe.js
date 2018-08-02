47d0d00ec9d9523ac3431f9eb33f9b92444c60a3
Denny Huayta <denny.huayta@sap.com>
2018-08-01 19:37:25 -0500

    Agregar tabla y funcion para validar is un texto se encuentra en una cadena

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,22 @@
 "use strict"
+
+/**
+ * Cadena con los valores de las celdas que dio click
+ */
+var usuarioclick = "12345";
+
+/**
+ * Para validar si un numero esta en la cadema usuariockik
+ * @param {*} numero 
+ */
+function validar(numero){
+    // indexOf devuelve la posision de la cadema buscada
+    var index = usuarioclick.indexOf(numero);
+    if (index = -1){
+        console.log("no tiene ese valor: " + numero);
+        return false;
+    } else {
+        console.log("Si tiene el valor: " + numero);
+        return true;
+    }
+}
